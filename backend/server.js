13ccaf358997462247766f3507484d90a22f97b2
Yasith <yasithnawodya9@gmail.com>
2023-11-17 22:49:08 +0530

    middleware test routes

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,29 @@
 require('dotenv').config();
 
 const express = require('express');
+const workoutRoutes = require('./routes/workouts.js')
 
 
 //creatd express app
 const app = express();
 
+//register the midleware()
+//any code that execute between getting request on the server and sending the response
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+app.use(express.json())
+
+
 //routing
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
     res.json({ msg: "welcome to the application" })
 })
+ */
+
+app.use('/api/workouts', workoutRoutes);
 
 
 //port listing
